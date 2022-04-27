31a81330adbf1f0de2cf7ad49734c87ce8c8621c
cedcoss <sujeetsahu@cedcommerce.com>
2022-04-27 18:39:10 +0530

    counter java script

---
+++ counter.js
@@ -0,0 +1,13 @@
+var n;
+n=0;
+function increment()
+{
+   n=n+1;
+   document.getElementById("middiv").innerHTML=n;
+}
+
+function decrement()
+{
+    n=n-1;
+    document.getElementById("middiv").innerHTML=n;
+}
