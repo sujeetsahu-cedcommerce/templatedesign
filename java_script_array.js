14c389d3ce55bed1cfc79d8590cac952589db0a0
cedcoss <sujeetsahu@cedcommerce.com>
2022-04-29 18:50:05 +0530

    create a java script array to store the information

--- java_script_array.js
+++ java_script_array.js
@@ -1,9 +1,9 @@
 const product = [
     ['Samsung' , 'Galaxy' , 64 , 1500],
     ['nokia' , 'S730' ,128, 22000],
-    ['xiaomi' , 'note' , 32 , 12000],
-    ['motorola' , 'g10', 32 , 14000],
-    ['apple' , 's12' , 64 , 18000]
+    ['xiaomi' , 'Note' , 32 , 12000],
+    ['motorola' , 'G10', 32 , 14000],
+    ['apple' , 'S12' , 64 , 18000]
 ] 
 
 //  console.table(product);
@@ -23,4 +23,39 @@ txt+="</table>";
 //alert(text);
 //document.write("Hello");
 document.getElementById("divDisp").innerHTML=txt;
+
+
+
+function mysearch()
+{
+    var select = document.getElementById("selectitem").value;
+    var search = document.getElementById("searchitem").value;
+
+      console.log(select);
+      console.log(search);
+  
+     
+    var text="<table><tr><th>Company</th><th>Model</th><th>Memory</th><th>Price</th></tr>";
+
+    for( let i=0; i<=4; i++)
+    {
+        for( let j=select; j<=select; j++)
+        {
+            if(product[i][j]==search)
+            {   
+                text += "<tr>";
+                for( let k=0;k<=3;k++)
+                {
+                    var z = product[i][k];
+                    text+="<td>" + z + "</td>";
+                }
+                text += "</tr>";
+            }
+        }
+    }
+    text+="</table>";
+    document.getElementById("result").innerHTML=text;
+}
+
+
 
