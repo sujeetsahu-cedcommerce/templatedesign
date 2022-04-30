1f45631d63ec30afd933180046cf70b6d6fc64be
cedcoss <sujeetsahu@cedcommerce.com>
2022-04-30 12:35:03 +0530

    java script sorting accroding to ascending and select company

--- java_script_array.js
+++ java_script_array.js
@@ -1,9 +1,9 @@
 const product = [
     ['Samsung' , 'Galaxy' , 64 , 1500],
-    ['nokia' , 'S730' ,128, 22000],
-    ['xiaomi' , 'Note' , 32 , 12000],
-    ['motorola' , 'G10', 32 , 14000],
-    ['apple' , 'S12' , 64 , 18000]
+    ['Nokia' , 'S730' ,128, 22000],
+    ['Xiaomi' , 'Note' , 32 , 12000],
+    ['Motorola' , 'G10', 32 , 14000],
+    ['Apple' , 'S12' , 64 , 18000]
 ] 
 
 //  console.table(product);
@@ -29,12 +29,7 @@ document.getElementById("divDisp").innerHTML=txt;
 function mysearch()
 {
     var select = document.getElementById("selectitem").value;
-    var search = document.getElementById("searchitem").value;
-
-      console.log(select);
-      console.log(search);
-  
-     
+    var search = documdevice +="</table>";
     var text="<table><tr><th>Company</th><th>Model</th><th>Memory</th><th>Price</th></tr>";
 
     for( let i=0; i<=4; i++)
@@ -55,7 +50,69 @@ function mysearch()
     }
     text+="</table>";
     document.getElementById("result").innerHTML=text;
-}
+}   
 
+      var drop1 = document.getElementById("dropbox1").value;
+      var drop2 = document.getElementById("dropbox2").value;
+
+      console.log(drop1);
+      console.log(drop2);
+
+
+function sorting()
+{   
+    var drop1 = document.getElementById("dropbox1").value;
+    var drop2 = document.getElementById("dropbox2").value;
+    
+    console.log(drop1);
+    console.log(drop2);
+
+    
+    product.sort(sortFunction);
+    var device ="<table><tr><th>Company</th><th><Model></th><th>Memory</th><th>Price</th></tr>";
+    function sortFunction(a,b)
+     {
+          if(a[drop2] == b[drop2])
+          { 
+            
+            return 0;
+          }
+          else
+          {
+        
+            if(drop1==0)
+            { 
+                return (a[drop2]>b[drop2]) ? 1 : -1;
+               
+            }
+            else
+            {
+                return (a[drop2]>b[drop2]) ? -1 : 1;
+                
+            }
+           
+          }
+     }
+     displayTable();
+     function displayTable()
+     {
+     var txt="<table><tr><th>Company</th><th>Model</th><th>Memory</th><th>Price</th></tr>";
+     for (let i=0 ; i<=4 ;i++)
+     {
+       txt+="<tr>";
+       for (let j=0 ;j<=3 ;j++)
+       {
+          var x = product[i][j];
+        
+          txt+="<td>"+x+"</td>";
+       }  
+       txt+="</tr>";
+     }
+     txt+="</table>";
+//alert(text);
+//document.write("Hello");
+     document.getElementById("sortingproduct").innerHTML=txt;
+    }
+   }
 
 
