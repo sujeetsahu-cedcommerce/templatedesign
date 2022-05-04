c26082431c6e18c6f6781c3870e3138a22268ac7
cedcoss <sujeetsahu@cedcommerce.com>
2022-05-04 13:09:05 +0530

    Constructing Strings with Variables

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-let myStr ="This is the first sentence.";
-myStr += "This is the second sentence.";
+// Only change code below this line
+const myName = "sujeet";
+const myStr = "My name is " + myName + "and I am well!";
