91bf7c36c9bd5910a5969aee53cbe3b6e8719b82
cedcoss <sujeetsahu@cedcommerce.com>
2022-05-04 11:11:41 +0530

    Comment Your JavaScript Code

---
+++ index.js
@@ -0,0 +1,5 @@
+
+  //It is a single line comment;
+  /*There are two ways to write comments in JavaScript:
+
+Using  will tell JavaScript to ignore the remainder of the text on the current line. This is an in-line comment:*/
