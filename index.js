95993826e7d7f824b371927f5e42b35cbd48b1ee
cedcoss <sujeetsahu@cedcommerce.com>
2022-05-04 18:46:00 +0530

    Stand in Line

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-// Setup
-let processed = 0;
-processed = processArg(7);
-function processArg(num) {
-  return (num + 3) / 5;
-}
-
-// Only change code below this line
+function nextInLine(arr, item ) {
+    // Only change code below this line
+    arr.push(item);
+   
+    return  arr.shift();
+    // Only change code above this line
+  }
+  
+  // Setup
+  const testArr = [1, 2, 3, 4, 5];
+  
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
