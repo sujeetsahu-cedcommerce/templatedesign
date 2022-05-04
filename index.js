36a19f5e47606296a4f55bb7018affafd0eda8fd
cedcoss <sujeetsahu@cedcommerce.com>
2022-05-04 11:20:13 +0530

    Declare JavaScript Variables

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-
-  //It is a single line comment;
-  /*There are two ways to write comments in JavaScript:
-
-Using  will tell JavaScript to ignore the remainder of the text on the current line. This is an in-line comment:*/
+var myname;
