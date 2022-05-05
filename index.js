338d10383ba019fe238aaa98bde1624ae879a884
cedcoss <sujeetsahu@cedcommerce.com>
2022-05-05 14:53:31 +0530

    Use the Conditional (Ternary) Operator

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function convertToInteger(str) {
-  
-    return parseInt(str ,2);
-  }
-  
-  convertToInteger("10011");
+function checkEqual(a, b) {
+    
+    return a == b ? "Equal" : "Not Equal";
+}
+
+checkEqual(1, 2);
