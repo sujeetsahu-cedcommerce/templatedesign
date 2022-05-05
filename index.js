4e0c0d99168496d4efe2169a68ada63e5fa26dc5
cedcoss <sujeetsahu@cedcommerce.com>
2022-05-05 10:51:25 +0530

    Counting Cards

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
+let count = 0;
 
-
- 
-
-// Setup
-
-function abTest(a, b) {
-
-    // Only change code below this line
-
-     if( a < 0 || b < 0)
-
-        return;
-
-    // Only change code above this line
-
- 
-
-    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
-
+function cc(card) {
+  // Only change code below this line
+  switch(card)
+  {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count--;
+      break;
   }
+  if(count > 0)
+      return(count +" "+ "Bet");
+  return(count +" "+ "Hold");
+  // Only change code above this line
+}
 
-  abTest(2,20);
+cc(2); cc(3); cc(7); cc('K'); cc('A');
