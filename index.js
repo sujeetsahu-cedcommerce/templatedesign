1a9f7d874c66057b8f4eaf7af5b67757b999337f
cedcoss <sujeetsahu@cedcommerce.com>
2022-05-05 11:19:56 +0530

    Add New Properties to a JavaScript Object

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-// Setup
 const myDog = {
-    "name": "Coder",
+    "name": "Happy Coder",
     "legs": 4,
     "tails": 1,
     "friends": ["freeCodeCamp Campers"]
   };
   
-  // Only change code below this line
-  // myDog.name= "Happy Coder";
+  // myDog.bark="woof";
+  myDog["bark"]="woof";
   
-  myDog["name"] = "Happy Coder";
