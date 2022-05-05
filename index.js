b91ad07bbf4d66b131e3ea3a106c7a86bfe95a3e
cedcoss <sujeetsahu@cedcommerce.com>
2022-05-05 11:56:40 +0530

    Manipulating Complex Objects

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-function checkObj(obj, checkProp) {
-    if (obj.hasOwnProperty(checkProp)) {
-      return obj[checkProp];
-    } else {
-      return "Not Found";
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },  
+    {
+      artist: "Deep Purple",
+      title: "Smoke on the water",
+      release_year: 1976,
+      formats: ["CD", "8T", "LP"]
     }
-  }
+  ];
