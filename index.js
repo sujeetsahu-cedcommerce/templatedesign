3368b8f02df892c3ddccdd206fc497ddf34bbcf2
cedcoss <sujeetsahu@cedcommerce.com>
2022-05-05 10:22:45 +0530

    Returning Boolean Values from Functions

--- index.js
+++ index.js
@@ -1,49 +1,15 @@
-//   Replacing If Else Chains with Switch
+// Returning Boolean Values from Functions
 
-function chainToSwitch(val) {
+function isLess(a, b) {
 
-    let answer = "";
-  
     // Only change code below this line
-  
-    switch(val)
-  
-    {
-  
-      case "bob":
-  
-        answer = "Marley";
-  
-        break;
-  
-      case 42:
-  
-        answer ="The Answer";
-  
-        break;
-  
-      case 1:
-  
-        answer = "There is no #1";
-  
-        break;
-  
-      case 99:
-  
-        answer = "Missed me by this much!";
-  
-        break;
-  
-      case 7:
-  
-        answer = "Ate Nine";
-  
+
+     return a < b;
+
     }
-  
+
     // Only change code above this line
-  
-    return answer;
-  
-  }
-  
-  chainToSwitch(7);
+
+ 
+
+  isLess(10, 15);
