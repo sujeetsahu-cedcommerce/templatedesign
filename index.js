11ce8a48460705265245dd99181d543e52014c8b
cedcoss <sujeetsahu@cedcommerce.com>
2022-05-05 11:49:15 +0530

    Testing Objects for Properties

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-// Setup
-function phoneticLookup(val) {
-    let result = "";
-  
-    // Only change code below this line
-    const lookup = {
-      "alpha":"Adams",
-      "bravo":"Boston",          
-      "delta":"Denver",
-      "charlie":"Chicago",
-      "echo":"Easy",
-      "foxtrot":"Frank"
-    };
-    result=lookup[val];
-    // Only change code above this line
-    return result;
+function checkObj(obj, checkProp) {
+    if (obj.hasOwnProperty(checkProp)) {
+      return obj[checkProp];
+    } else {
+      return "Not Found";
+    }
   }
-  
-  phoneticLookup("charlie");
